46e2ba7da90c25725eec4ed8b10205f6482ed77e
Liam Davies <lmdudester@gmail.com>
2017-09-17 13:42:46 -0400

    Add documentation and more methods

--- cardGames/structures/card.py
+++ cardGames/structures/card.py
@@ -1,10 +1,28 @@
+# class Card
+#   Object: Card - Represents a single card (as in a deck of cards)
+# Shared class data:
+#   faceDict - A dictionary mapping the number representing
+#               the face value to a string
+#   suitList - A list mapping the number representing
+#               the suit value to a string
+# Attributes:
+#   face - a number 2-14 representative of the face value of the card
+#   suit - a number 0-3 representative of the suit of the card
 class Card:
-    #Has attributes "suit" and "face"
     faceDict = {2:"Two", 3:"Three", 4:"Four", 5:"Five", 6:"Six", 7:"Seven", 8:"Eight",
                 9:"Nine", 10:"Ten", 11:"Jack", 12:"Queen", 13:"King", 14:"Ace"}
 
     suitList = ["Diamonds", "Hearts", "Spades", "Clubs"]
 
+    # **__init()__**
+    # Creates a Card object and throws an error if it was given an invalid argument
+    # Args:
+    #   face - a number 2-14 representative of the face value of the card
+    #   suit - a number 0-3 representative of the suit of the card
+    # Return:
+    #   N/A
+    # RaiseError:
+    #   If an invalid argument value was sent
     def __init__(self, face, suit):
         if(suit < 0 or suit > 3):
             raise ValueError("Invalid Suit Number: %i", suit)
@@ -14,5 +32,29 @@ class Card:
             raise ValueError("Invalid Face Number: %i", face)
         self.face = face #2-14
 
-    def toStr(self):
+    # **getFaceString()**
+    # Returns a string representing the face value of the card
+    # Args:
+    #   N/A
+    # Return:
+    #   A string of the face value of the card object
+    def getFaceString(self):
+        return Card.faceDict[self.face]
+
+    # **getSuitString()**
+    # Returns a string representing the suit of the card
+    # Args:
+    #   N/A
+    # Return:
+    #   A string of the suit of the card object
+    def getSuitString(self):
+        return Card.suitList[self.suit]
+
+    # **toString()**
+    # Returns a string representing the face and suit of the card
+    # Args:
+    #   N/A
+    # Return:
+    #   A string of the suit of the format "<face> of <suit>"
+    def toString(self):
         return Card.faceDict[self.face] + " of " + Card.suitList[self.suit]
--- cardGames/structures/deck.py
+++ cardGames/structures/deck.py
@@ -1,8 +1,21 @@
 from structures.card import Card
 from random import randint
 
+# class Deck
+#   Object: Deck - Represents a 52 Card Deck
+# Shared class data:
+#   N/A
+# Attributes:
+#   deckList - the list of all 52 of the cards in the deck
+#   index - the index in the current deckList that represents the top card
 class Deck:
 
+    # **__init()__**
+    # Creates a Deck object and shuffles it
+    # Args:
+    #   N/A
+    # Return:
+    #   N/A
     def __init__(self):
         self.index = 0
         self.deckList = []
@@ -12,6 +25,12 @@ class Deck:
                 self.deckList.append(Card(i,j))
         self.shuffle()
 
+    # **shuffle()**
+    # Randomly shuffles the list of cards
+    # Args:
+    #   N/A
+    # Return:
+    #   N/A
     def shuffle(self):
         self.index = 0
         for x in range(0, 50):
@@ -20,12 +39,49 @@ class Deck:
             self.deckList[x] = self.deckList[num]
             self.deckList[num] = temp
 
+    # **peekCard()**
+    # Gets a card object at a given index in the reamining deck without
+    #   altering the top card of the deck
+    # Args:
+    #   subIndex - the index of the card in the remaining deck (0-maxIndex)
+    # Return:
+    #   The card at that sub-index
+    def peekCard(self, subIndex):
+        if(self.index + subIndex >= 52):
+            return None
+        else:
+            return self.deckList[self.index + subIndex]
+
+    # **getTopCard()**
+    # Gets the "top" card object of the deck and edits the deck to
+    #   reflect that
+    # Args:
+    #   N/A
+    # Return:
+    #   The "top" card object of the deck or None if the deck is empty
     def getTopCard(self):
+        if(self.index == 52):
+            return None
         self.index = self.index + 1
         return self.deckList[self.index - 1]
 
-    def toStr(self):
+    # **size()**
+    # Gets the number of cards remaining in the deck
+    # Args:
+    #   N/A
+    # Return:
+    #   The number of cards remaining in the deck
+    def size(self):
+        return 52 - self.index
+
+    # **toString()**
+    # Gets a string representing the whole deck
+    # Args:
+    #   N/A
+    # Return:
+    #   A string of the format: "<face> of <suit>\n<face> of <suit>\n..."
+    def toString(self):
         ret = ""
         for i in range(self.index, 52):
-            ret = ret + self.deckList[i].toStr() +"\n"
+            ret = ret + self.deckList[i].toString() +"\n"
         return ret;
